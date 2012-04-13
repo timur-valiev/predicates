add2beba5d940c0f376c986ad82010bc046f82af
Timur Valiev <zloi.timur@gmail.com>
2012-04-13 13:07:24 +0400

    predicate service

---
+++ src/main/java/predicates/PredicateService.java
@@ -0,0 +1,16 @@
+package predicates;
+
+import predicates.domain.Func;
+import predicates.domain.Predicate;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Тимур
+ * Date: 13.04.12
+ * Time: 13:05
+ */
+public class PredicateService {
+    public static boolean checkSave(Predicate predicate, Func func){
+        return false;
+    }
+}
