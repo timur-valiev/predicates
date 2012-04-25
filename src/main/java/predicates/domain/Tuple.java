54b945979ebe4b9b74c5bec9be500db759c050e3
Timur Valiev <zloi.timur@gmail.com>
2012-04-25 16:23:13 +0400

    tuple added

---
+++ src/main/java/predicates/domain/Tuple.java
@@ -0,0 +1,60 @@
+package predicates.domain;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Тимур
+ * Date: 25.04.12
+ * Time: 16:06
+ */
+public class Tuple implements Iterable<Tuple>, Iterator<Tuple> {
+    private int dim;
+    private List<Integer> current;
+    private List<Integer> next;
+
+
+    public Tuple(int dim, int capacity) {
+        this.dim = dim;
+        next =  Arrays.asList(new Integer[capacity]);
+    }
+
+    public Integer getValue(int code){
+        return current.get(code);
+    }
+
+    @Override
+    public boolean hasNext() {
+        return next != null;
+    }
+
+    @Override
+    public Tuple next() {
+        current = new ArrayList<Integer>(next);
+        int i = 0;
+        boolean changed = false;
+        while (i < next.size() && !changed){
+            if (next.get(i) == dim-1){
+                next.set(i,0);
+                i++;
+            } else {
+                next.set(i, next.get(i)+1);
+                changed = true;
+            }
+        }
+        if (!changed)
+            next = null;
+        return this;
+    }
+
+    @Override
+    public Iterator<Tuple> iterator() {
+        return this;
+    }
+
+    @Override
+    public void remove() {}
+}
